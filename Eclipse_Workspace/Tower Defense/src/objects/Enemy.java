c386eb96bc3f462c54d6edca28a32adad9e1451d
SenorBlob <jonasschroeder23@hotmail.de>
2018-01-07 16:33:08 +0100

    Some sort of first implementation of the Enemie Class(just an early)

--- Eclipse_Workspace/Tower Defense/src/objects/Enemy.java
+++ Eclipse_Workspace/Tower Defense/src/objects/Enemy.java
@@ -1,5 +1,64 @@
 package objects;
 
-public class Enemie {
+/*
+ * The Enemy Class is just apparent as the Enemy.
+ * The movement is controlled by the Enemy_Controller.
+ */
+public class Enemy {
+	
+	private Integer posX;
+	private Integer posY;
+	
+	private Double life;
+	private Double speed;
+	
+	private Integer cost;
+	
+	/*
+	 * 
+	 */
+	public Enemy() {
+		
+	}
 
+	public Integer getPosX() {
+		return posX;
+	}
+
+	public void setPosX(Integer posX) {
+		this.posX = posX;
+	}
+
+	public Integer getPosY() {
+		return posY;
+	}
+
+	public void setPosY(Integer posY) {
+		this.posY = posY;
+	}
+
+	public Double getLife() {
+		return life;
+	}
+
+	public void setLife(Double life) {
+		this.life = life;
+	}
+
+	public Double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Double speed) {
+		this.speed = speed;
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+	
 }
