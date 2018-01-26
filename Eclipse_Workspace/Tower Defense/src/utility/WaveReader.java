3d9f29db6ba8ce193fa6a72e8a770bffdd2f337c
SenorBlob <jonasschroeder23@hotmail.de>
2018-01-26 16:17:21 +0100

    Edited the Wave Reading System

--- Eclipse_Workspace/Tower Defense/src/utility/WaveReader.java
+++ Eclipse_Workspace/Tower Defense/src/utility/WaveReader.java
@@ -3,59 +3,75 @@ package utility;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 
  */
 public class WaveReader {
-	
+
 	/**
 	 * File structure is the following:
-	 * 		
-	 * 			[Number of Enemies],[Enemies Type],[Enemies Level];
+	 * 
+	 * [Number of Enemies],[Enemies Type],[Enemies Level];
 	 */
-	private String fileContent;
+
+	private ArrayList<Wave> waveList = new ArrayList<Wave>();
+
+	public static final String COMMENT_START = "/";
 
 	/**
 	 * 
 	 */
 	public WaveReader() {
-		
+
 	}
-	
+
 	/**
 	 * 
 	 */
-	public void loadWaveFile(String fileName) {
-		
-        try {
-            FileReader file = new FileReader(fileName);
-            BufferedReader  br = new BufferedReader(file);
-            
-            String input = "";
-            
-            while((input = br.readLine()) != null) {
-                fileContent = fileContent + input;
-            }
-            
-            br.close();
-        } catch(IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-	
-	/**
-	 * uses Regular Expression to process the Lines of the File into the requested format
-	 */
-	public void processFileContent() {
-		
-	}
-	
-	public String getFileContent() {
-		return fileContent;
+	public ArrayList<Wave> loadWaveFile(String fileName) {
+		System.out.println("Searching in File");
+		try {
+			FileReader file = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(file);
+
+			String input = "";
+
+			while ((input = br.readLine()) != null) {
+				System.out.println(input);
+				if (!input.startsWith(WaveReader.COMMENT_START)) {
+					processFileContent(input);
+				}
+			}
+
+			br.close();
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		return waveList;
 	}
 
-	public void setFileContent(String fileContent) {
-		this.fileContent = fileContent;
+	/**
+	 * uses Regular Expression to process the Lines of the File into the requested
+	 * format
+	 */
+	public void processFileContent(String pInput) {
+		String regex = "(\\d+)\\s*,(\\d+)\\s*,(\\d+)\\s*;";
+		Pattern pattern = Pattern.compile(regex);
+		System.out.println(pattern.toString());
+		Matcher matcher = pattern.matcher(pInput);
+		if (matcher.matches()) {
+			int numberOfEnemies = Integer.valueOf(matcher.group(1));
+			System.out.println(numberOfEnemies);
+			int typeOfEnemies = Integer.valueOf(matcher.group(2));
+			System.out.println(typeOfEnemies);
+			int levelOfEnemies = Integer.valueOf(matcher.group(3));
+			System.out.println(levelOfEnemies);
+			Wave nW = new Wave(numberOfEnemies, typeOfEnemies, levelOfEnemies);
+			this.waveList.add(nW);
+		}
 	}
 }
