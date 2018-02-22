0499e1d288636ad9e93644bff33e3aa03c93710c
simon.lukas <simon.lukas@q235-15.schule.local>
2018-02-22 15:49:47 +0100

    "Clock update"

--- Eclipse_Workspace/Tower Defense/src/game_Controller/Clock.java
+++ Eclipse_Workspace/Tower Defense/src/game_Controller/Clock.java
@@ -1,20 +1,20 @@
 package game_Controller;
 
-//Begriffe und Abkürzungen / Glossar:
-//(bps = Bilder pro Sekunde, Part = Thread, X)
-
 public class Clock {
-	@SuppressWarnings("unused")
-	private int bps = 60;
-	private int bZähler = 0;
-	private boolean läuft = true; // Müssen geregelt werden.
-	private boolean pausiert = false; // Müssen geregelt werden
+
+	private boolean läuft = true;
+	private boolean pausiert = false;
+
+	public static void main(String args[]) {
+		Clock c = new Clock();
+		c.laufenderSpielRing();
+	}
 
 	public Clock() {
 
 	}
 
-	// Startet einen neuen Part. Führt die Methode spielRing() aus.
+	//Create a new Thread running the Game Loop
 	public void laufenderSpielRing() {
 		Thread ring = new Thread() {
 
@@ -27,57 +27,35 @@ public class Clock {
 		ring.start();
 	}
 
-	// Nur in einem anderen Thread ausführen!
+	//main construct
 	public void spielRing() {
 
 		final double wiederholrate = 30.0;
-
-		// Schätzt ein wie viel Zeit benötigt wird um die angepeilten bps zu erreichen!
-		final double zeitZwischenAktualisierungen = 1000000000 / wiederholrate;
 		final int maxAktualisierungen = 1;
 
-		double lastUpdateTime = System.nanoTime();
-
-		// Einfacher Weg um die bps festzustellen.
-		int lastSecondTime = (int) (lastUpdateTime / 1000000000);
-
-		// Hauptkonstrukt
+		
+		
 		while (läuft) {
-			double now = System.nanoTime();
-			int updateCount = 0;
+
+			try {
+				Thread.sleep((long) (maxAktualisierungen / wiederholrate * 1000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 
 			if (!pausiert) {
-
-				// Soviele Aktualisierungen wie nötig machen. (Wie viele das sein werden bereits
-				// in Zeile 41 festgelegt(1 um visuelle Fehler zu vermeiden))
-				while (now - lastUpdateTime > zeitZwischenAktualisierungen && updateCount < maxAktualisierungen) {
+				
 					aktualisierung();
-					lastUpdateTime += zeitZwischenAktualisierungen;
-					updateCount++;
-				}
 
-				// Aktualisierung der bereits erstellten Bilder.
-				int thisSecond = (int) (lastUpdateTime / 1000000000);
-				if (thisSecond > lastSecondTime) {
-					System.out.println("NEW SECOND " + thisSecond + " " + bZähler);
-					bps = bZähler;
-					bZähler = 0;
-					lastSecondTime = thisSecond;
 				}
-
-			}
 		}
 
 	}
 
-	// Integration der Aktualisierungen der Spielmechanik!
+	//Integrate mechanics
 	public void aktualisierung() {
-
-	}
-
-	public static void main(String args[]) {
-		Clock c = new Clock();
-		c.laufenderSpielRing();
+		
+		// System.out.println("Pathing Complete");
 	}
 
 }
