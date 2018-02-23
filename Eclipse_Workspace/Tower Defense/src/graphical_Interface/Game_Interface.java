bebeb4d776a903c22c0e55f208b131fe338d3575
Simon Lukas <34345033+JustBrainlessBI@users.noreply.github.com>
2018-02-23 19:36:06 +0100

    "Mache morgen weiter quetsche mich erstmal ein wenig in die swing Biblio"

--- Eclipse_Workspace/Tower Defense/src/graphical_Interface/Game_Interface.java
+++ Eclipse_Workspace/Tower Defense/src/graphical_Interface/Game_Interface.java
@@ -1,32 +1,44 @@
 package graphical_Interface;
 
-import java.util.Date;
-import java.io.PrintStream;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class Game_Interface{
-
-	static PrintStream OUT = System.out;
+public class Game_Interface {
 
 	public static void main(String[] args) {
-		
-		SwingUtilities.invokeLater(new Runnable() 
-		{
-			public void run() 
-			{
+
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
 				startGUI();
 			}
 		});
 	}
 
-	private static void startGUI() 
-	{
+	//Setup the GUI
+	private static void startGUI() {
 		JFrame window = new JFrame();
+		setupContent(window);
+		//IconJFrame(window);
 		window.setTitle("Master Defense");
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		window.setUndecorated(true);
 		window.setVisible(true);
+		window.setResizable(false);
+	}
+	
+	//Setup the GUI functions.
+	private static void setupContent(JFrame window) {
+		JPanel content = new JPanel();
+		window.setContentPane(content);
+	}
+
+	//Just a bit fun :D
+	private static void IconJFrame(JFrame window) {
+		ImageIcon icon;
+		icon = new ImageIcon("");			//Still need a path an a picture :(
+		setIconImage(icon.getImage());
 	}
 
 }
