c5b9be529982283a74828b5c822f052fc4b0f604
SenorBlob <jonasschroeder23@hotmail.de>
2018-02-24 00:10:58 +0100

    "Changed the apparently not taken by GitHub"

--- Eclipse_Workspace/Tower Defense/src/game_Controller/Game_Controller.java
+++ Eclipse_Workspace/Tower Defense/src/game_Controller/Game_Controller.java
@@ -269,6 +269,7 @@ public class Game_Controller {
 				while(!gC.getEnemyList().isEmpty()) {
 					for (int o = 0; o < gC.getEnemyList().size(); o++) {
 						Enemy current = gC.getEnemyList().get(o);
+						
 						if (gC.getEnemyController().isMovable(current)) {
 							if(current.checkAlife()) {
 	//If Tower in Range
