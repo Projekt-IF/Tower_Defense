c64fa3c437334a00df2203979ae2dfa26a70cf74
simon.lukas <simon.lukas@q235-13.schule.local>
2018-03-22 16:31:32 +0100

    TextGui ;D fuck hello world

---
+++ Eclipse_Workspace/Tower Defense/src/graphical_Interface/TD_Client.java
@@ -0,0 +1,17 @@
+package graphical_Interface;
+
+
+public class TD_Client extends Client {
+	
+	private TextGui myGui;
+	
+	public TD_Client(String pServerIP, int pServerPort, TextGui tg){
+		super(pServerIP, pServerPort);
+		this.myGui = tg;
+
+	}
+	
+	public void processMessage(String pMessage){
+		this.myGui.ausgeben(pMessage);
+	}
+}
