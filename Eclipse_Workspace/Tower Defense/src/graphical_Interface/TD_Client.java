c87ae68c513951ef206850026065b0c33ac645b1
simon.lukas <simon.lukas@q235-13.schule.local>
2018-04-19 15:49:00 +0200

    "TD_Client Changes (Switch Statement)"

--- Eclipse_Workspace/Tower Defense/src/graphical_Interface/TD_Client.java
+++ Eclipse_Workspace/Tower Defense/src/graphical_Interface/TD_Client.java
@@ -15,8 +15,88 @@ public class TD_Client extends Client {
 	public void processMessage(String pMessage) {
 		String[] tags = pMessage.split(Protocol.SEPARATOR);
 		String prefix = tags[0];
-		if(tags[1] != null) {
-		this.myGui.ausgeben(tags[1]);
+		
+		switch(prefix) {
+		
+		case Protocol.SC_HELLO_WORLD:
+			this.myGui.ausgeben(tags[1]);
+			break;
+		
+		//Not game important communication
+		
+		case Protocol.SC_LOGIN_CONFIRMED:
+			break;
+			
+		case Protocol.SC_LOGOUT_CONFIRMED:
+			break;
+			
+		case Protocol.SC_PING_BACK:
+			break;
+			
+		case Protocol.SC_RECONNECT_PLAYER:
+			break;
+			
+		case Protocol.SC_KICK_PLAYER:
+			break;
+			
+		//In-Game communication
+			
+		case Protocol.SC_GAME_STARTING:
+			break;
+		
+		case Protocol.SC_LOAD_ENEMIES:
+			break;
+			
+		case Protocol.SC_LOAD_MAP:
+			break;
+			
+		case Protocol.SC_LOAD_TOWER:
+			break;
+		
+		//Graphical Changes to be made by the Client
+			
+			
+		case Protocol.SC_UPDATE_PLAYER_MONEY:
+			break;
+			
+		case Protocol.SC_UPDATE_POSITION_ENEMY:
+			break;
+			
+		case Protocol.SC_UPDATE_POSITION_TOWER:
+			break;
+			
+		case Protocol.SC_CHANGE_MAP:
+			break;
+			
+		case Protocol.SC_REMOVE_ENEMY:
+			break;
+			
+		case Protocol.SC_REMOVE_TOWER:
+			break;
+			
+		//End of game messages
+			
+		case Protocol.SC_VICTORY:
+			break;
+			
+		case Protocol.SC_LOSS:
+			break;
+			
+		//Universal
+			
+		case Protocol.SC_SENDERRORMESSAGE:
+			break;
+			
+		case Protocol.SC_SURRENDER_SUCCESSFUL:
+			break;
+			
+		case Protocol.SC_SURRENDER_UNSUCCESSFUL:
+			break;
+			
+		case Protocol.SEPARATOR:
+			break;
+		
+		
 		}
 	}
 }
